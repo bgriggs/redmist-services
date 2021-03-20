178a852860f2683b67b44800e17cfd7808072811
Brian Griggs <bgriggs@econolite.com>
2021-03-20 15:47:03 -0600

    Adding redis support to channel status service #245

--- aspnet-core/services/BigMission.RealTimeStatusProcessor/Application.cs
+++ aspnet-core/services/BigMission.RealTimeStatusProcessor/Application.cs
@@ -7,6 +7,7 @@ using BigMission.ServiceStatusTools;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using NLog;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -32,6 +33,12 @@ namespace BigMission.CarRealTimeStatusProcessor
         private BigMissionDbContext context;
         private readonly ManualResetEvent serviceBlock = new ManualResetEvent(false);
 
+        private volatile bool useCache;
+        private ConnectionMultiplexer cacheMuxer;
+        private const string CHANNEL_KEY = "ch{0}";
+        private const string CHANNEL_HIST_KEY = "chist{0}";
+        private const int HIST_MAX_LEN = 60;
+
 
         public Application(IConfiguration config, ILogger logger, ServiceTracking serviceTracking)
         {
@@ -46,6 +53,14 @@ namespace BigMission.CarRealTimeStatusProcessor
         {
             ServiceTracking.Update(ServiceState.STARTING, string.Empty);
 
+            // Attempt to connect to redis cache
+            useCache = !string.IsNullOrEmpty(Config["RedisConn"]);
+            Logger.Info($"Cache available={useCache}");
+            if (useCache)
+            {
+                GetCache();
+            }
+
             var cf = new BigMissionDbContextFactory();
             context = cf.CreateDbContext(new[] { Config["ConnectionString"] });
 
@@ -54,8 +69,11 @@ namespace BigMission.CarRealTimeStatusProcessor
             Task receiveStatus = ehReader.ReadEventHubPartitionsAsync(Config["KafkaConnectionString"], Config["KafkaDataTopic"], Config["KafkaConsumerGroup"],
                 partitionFilter, EventPosition.Latest, ReceivedEventCallback);
 
-            // Process the cached status and update the database
-            saveTimer = new Timer(SaveCallback, null, 2000, 300);
+            if (!useCache)
+            {
+                // Process the cached status and update the SQL database
+                saveTimer = new Timer(SaveCallback, null, 2000, 300);
+            }
 
             // Start updating service status
             ServiceTracking.Start();
@@ -68,6 +86,7 @@ namespace BigMission.CarRealTimeStatusProcessor
         {
             try
             {
+                var sw = Stopwatch.StartNew();
                 var json = Encoding.UTF8.GetString(receivedEvent.Data.Body.ToArray());
                 var chDataSet = JsonConvert.DeserializeObject<ChannelDataSet>(json);
 
@@ -76,9 +95,33 @@ namespace BigMission.CarRealTimeStatusProcessor
                     chDataSet.Data = new ChannelStatus[] { };
                 }
 
-                Logger.Trace($"Received log: {chDataSet.DeviceAppId}");
+                Logger.Trace($"Received log: {chDataSet.DeviceAppId} Count={chDataSet.Data.Length}");
+                if (!chDataSet.Data.Any()) { return; }
 
-                // Update cached status
+                if (useCache)
+                {
+                    var kvps = new List<KeyValuePair<RedisKey, RedisValue>>();
+                    foreach (var ch in chDataSet.Data)
+                    {
+                        var cs = new CacheModels.ChannelStatus { Value = ch.Value, Timestamp = ch.Timestamp };
+                        var v = JsonConvert.SerializeObject(cs);
+                        var kvp = new KeyValuePair<RedisKey, RedisValue>(string.Format(CHANNEL_KEY, ch.ChannelId), v);
+                        kvps.Add(kvp);
+                    }
+                    var db = GetCache();
+                    if (db != null)
+                    {
+                        db.StringSet(kvps.ToArray(), flags: CommandFlags.FireAndForget);
+                        Logger.Trace($"Cached new status for device: {chDataSet.DeviceAppId}");
+                    }
+                    else
+                    {
+                        Logger.Warn("Cache was not available, failed to update status.");
+                    }
+                }
+
+                // Update in-memory status locally
+                var history = new List<KeyValuePair<RedisKey, RedisValue>>();
                 lock (last)
                 {
                     foreach (var ch in chDataSet.Data)
@@ -88,21 +131,48 @@ namespace BigMission.CarRealTimeStatusProcessor
                             ch.DeviceAppId = chDataSet.DeviceAppId;
                         }
 
+                        // Append changed value to the moving channel history list
                         if (last.TryGetValue(ch.ChannelId, out ChannelStatus row))
                         {
                             if (row.Value != ch.Value)
                             {
                                 row.Value = ch.Value;
-                                row.Timestamp = ch.Timestamp;
+                                var p = CreateCacheEntry(ch);
+                                history.Add(p);
                             }
+
+                            // Keep timestamp current when we get an update
+                            row.Timestamp = ch.Timestamp;
                         }
                         else // Create new row
                         {
                             var cr = new ChannelStatus { DeviceAppId = ch.DeviceAppId, ChannelId = ch.ChannelId, Value = ch.Value, Timestamp = ch.Timestamp };
                             last[ch.ChannelId] = cr;
+                            var p = CreateCacheEntry(ch);
+                            history.Add(p);
                         }
                     }
                 }
+
+                if (useCache && history.Any())
+                {
+                    var db = GetCache();
+                    if (db != null)
+                    {
+                        foreach (var h in history)
+                        {
+                            // Use the head of the list as the newest value
+                            var len = db.ListLeftPush(h.Key, h.Value);
+                            if (len > HIST_MAX_LEN)
+                            {
+                                db.ListTrim(h.Key, 0, HIST_MAX_LEN - 1, flags: CommandFlags.FireAndForget);
+                            }
+                        }
+                        Logger.Trace($"Cached new history for device: {chDataSet.DeviceAppId}");
+                    }
+                }
+
+                Logger.Trace($"Processed status in {sw.ElapsedMilliseconds}ms");
             }
             catch (Exception ex)
             {
@@ -110,27 +180,17 @@ namespace BigMission.CarRealTimeStatusProcessor
             }
         }
 
-        public void UpdateChanges(ChannelStatus[] rows)
+        private static KeyValuePair<RedisKey, RedisValue> CreateCacheEntry(ChannelStatus ch)
         {
-            foreach (var updated in rows)
-            {
-                var r = context.ChannelStatus.SingleOrDefault(c => c.DeviceAppId == updated.DeviceAppId && c.ChannelId == updated.ChannelId);
-                if (r != null)
-                {
-                    r.Value = updated.Value;
-                    r.Timestamp = updated.Timestamp;
-                }
-                else
-                {
-                    context.ChannelStatus.Add(updated);
-                }
-            }
-
-            var sw = Stopwatch.StartNew();
-            context.SaveChanges();
-            Logger.Trace($"DB Commit in {sw.ElapsedMilliseconds}ms");
+            var st = new CacheModels.ChannelStatus { Value = ch.Value, Timestamp = ch.Timestamp };
+            var v = JsonConvert.SerializeObject(st);
+            var p = new KeyValuePair<RedisKey, RedisValue>(string.Format(CHANNEL_HIST_KEY, ch.ChannelId), v);
+            return p;
         }
 
+        /// <summary>
+        /// Take care of processing status updates to SQL on another thread.
+        /// </summary>
         private void SaveCallback(object obj)
         {
             if (Monitor.TryEnter(saveTimer))
@@ -156,6 +216,40 @@ namespace BigMission.CarRealTimeStatusProcessor
                     Monitor.Exit(saveTimer);
                 }
             }
+        }
+
+        /// <summary>
+        /// Save changes to SQL server.
+        /// </summary>
+        /// <param name="rows"></param>
+        public void UpdateChanges(ChannelStatus[] rows)
+        {
+            foreach (var updated in rows)
+            {
+                var r = context.ChannelStatus.SingleOrDefault(c => c.DeviceAppId == updated.DeviceAppId && c.ChannelId == updated.ChannelId);
+                if (r != null)
+                {
+                    r.Value = updated.Value;
+                    r.Timestamp = updated.Timestamp;
+                }
+                else
+                {
+                    context.ChannelStatus.Add(updated);
+                }
+            }
+
+            var sw = Stopwatch.StartNew();
+            context.SaveChanges();
+            Logger.Trace($"DB Commit in {sw.ElapsedMilliseconds}ms");
+        }
+
+        private IDatabase GetCache()
+        {
+            if (cacheMuxer == null || !cacheMuxer.IsConnected)
+            {
+                cacheMuxer = ConnectionMultiplexer.Connect(Config["RedisConn"]);
+            }
+            return cacheMuxer.GetDatabase();
         }
     }
 }
