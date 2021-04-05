08ca25520f328e9105bf1dd88ad1bab7d3e2a3d4
Brian Griggs <bgriggs@econolite.com>
2021-04-04 18:29:24 -0600

    Initial fuel stats #98

---
+++ aspnet-core/services/BigMission.FuelStatistics/Application.cs
@@ -0,0 +1,51 @@
+﻿using BigMission.CommandTools;
+using BigMission.EntityFrameworkCore;
+using BigMission.ServiceData;
+using BigMission.ServiceStatusTools;
+using Microsoft.Extensions.Configuration;
+using NLog;
+using StackExchange.Redis;
+using System.Threading;
+
+namespace BigMission.FuelStatistics
+{
+    /// <summary>
+    /// Processes application status from the in car apps. (not channel status)
+    /// </summary>
+    class Application
+    {
+        private IConfiguration Config { get; }
+        private ILogger Logger { get; }
+        private ServiceTracking ServiceTracking { get; }
+        private AppCommands Commands { get; }
+        private readonly EventHubHelpers ehReader;
+        private readonly ManualResetEvent serviceBlock = new ManualResetEvent(false);
+        private readonly ConnectionMultiplexer cacheMuxer;
+
+
+        public Application(IConfiguration config, ILogger logger, ServiceTracking serviceTracking)
+        {
+            Config = config;
+            Logger = logger;
+            ServiceTracking = serviceTracking;
+            Commands = new AppCommands(Config["ServiceId"], Config["KafkaConnectionString"], logger);
+            ehReader = new EventHubHelpers(logger);
+            cacheMuxer = ConnectionMultiplexer.Connect(Config["RedisConn"]);
+        }
+
+
+        public void Run()
+        {
+            ServiceTracking.Update(ServiceState.STARTING, string.Empty);
+
+            var cf = new BigMissionDbContextFactory();
+            using var db = cf.CreateDbContext(new[] { Config["ConnectionString"] });
+            
+
+            // Start updating service status
+            ServiceTracking.Start();
+            Logger.Info("Started");
+            serviceBlock.WaitOne();
+        }
+    }
+}
---
+++ aspnet-core/services/BigMission.FuelStatistics/Program.cs
@@ -0,0 +1,57 @@
+﻿using BigMission.ServiceStatusTools;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NLog;
+using NLog.Config;
+using System;
+using System.IO;
+
+namespace BigMission.FuelStatistics
+{
+    class Program
+    {
+        private static Logger logger;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var basePath = Directory.GetCurrentDirectory();
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                if (env.ToUpper() == "PRODUCTION")
+                {
+                    LogManager.Configuration = new XmlLoggingConfiguration($"{basePath}{Path.DirectorySeparatorChar}nlog.Production.config");
+                }
+                logger = LogManager.GetCurrentClassLogger();
+
+                logger.Info($"Starting env={env}...");
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(basePath)
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env}.json", optional: true)
+                    .Build();
+
+                var serviceStatus = new ServiceTracking(new Guid(config["ServiceId"]), "FuelStatistics", config["RedisConn"], logger);
+
+                var services = new ServiceCollection();
+                services.AddSingleton<NLog.ILogger>(logger);
+                services.AddSingleton<IConfiguration>(config);
+                services.AddSingleton(serviceStatus);
+                services.AddTransient<Application>();
+
+                var provider = services.BuildServiceProvider();
+
+                var application = provider.GetService<Application>();
+                application.Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
+        }
+    }
+}
