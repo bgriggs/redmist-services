4d0dd0b0061a62528136a2dfdc0be2faf47be4ac
Brian Griggs <brian@bigmissionmotorsports.com>
2023-06-26 19:43:05 -0600

    Updating cors

--- aspnet-core/services/BigMission.AlarmProcessor/Startup.cs
+++ aspnet-core/services/BigMission.AlarmProcessor/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using BigMission.TestHelpers;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,8 +10,6 @@ namespace BigMission.AlarmProcessor
 {
     public class Startup
     {
-        public Startup() { }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -18,6 +18,7 @@ namespace BigMission.AlarmProcessor
             services.AddHostedService<Application>();
             services.AddHealthChecks().AddCheck<ServiceHealthCheck>("service");
             services.AddControllers();
+            services.AddEndpointsApiExplorer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,13 +29,17 @@ namespace BigMission.AlarmProcessor
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseStaticFiles();
+            app.UseCors(builder => builder
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowAnyOrigin());
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapHealthChecks("/startup");
-                endpoints.MapHealthChecks("/liveness");
-                endpoints.MapHealthChecks("/ready");
+                endpoints.MapHealthChecks("/healthz", new HealthCheckOptions()
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
                 endpoints.MapControllers();
             });
         }
