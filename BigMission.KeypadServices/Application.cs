d005e3d6ea00d08b7b21ea21466cce717005d6a9
Brian Griggs <brian@bigmissionmotorsports.com>
2024-12-29 16:39:02 -0700

    Upgrade keypad to .net9

--- BigMission.KeypadServices/Application.cs
+++ BigMission.KeypadServices/Application.cs
@@ -2,81 +2,85 @@
 using BigMission.DeviceApp.Shared;
 using BigMission.ServiceStatusTools;
 using BigMission.TestHelpers;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using StackExchange.Redis;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace BigMission.KeypadServices
+namespace BigMission.KeypadServices;
+
+class Application : BackgroundService
 {
-    class Application : BackgroundService
+    private ILogger Logger { get; }
+    public IDateTimeHelper DateTime { get; }
+
+    private readonly IConnectionMultiplexer cacheMuxer;
+    private readonly StartupHealthCheck startup;
+
+    public Application(IConnectionMultiplexer cacheMuxer, ILoggerFactory loggerFactory, StartupHealthCheck startup, IDateTimeHelper dateTimeHelper)
     {
-        private ILogger Logger { get; }
-        public IDateTimeHelper DateTime { get; }
+        this.cacheMuxer = cacheMuxer;
+        this.startup = startup;
+        Logger = loggerFactory.CreateLogger(GetType().Name);
+        DateTime = dateTimeHelper;
+    }
 
-        private readonly IConnectionMultiplexer cacheMuxer;
-        private readonly StartupHealthCheck startup;
 
-        public Application(IConnectionMultiplexer cacheMuxer, ILoggerFactory loggerFactory, StartupHealthCheck startup, IDateTimeHelper dateTimeHelper)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Logger.LogInformation("Waiting for dependencies...");
+        while (!stoppingToken.IsCancellationRequested)
         {
-            this.cacheMuxer = cacheMuxer;
-            this.startup = startup;
-            Logger = loggerFactory.CreateLogger(GetType().Name);
-            DateTime = dateTimeHelper;
+            if (await startup.CheckDependencies())
+                break;
+            await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+        }
+        await startup.Start();
+
+        var sub = cacheMuxer.GetSubscriber();
+        await sub.SubscribeAsync(RedisChannel.Literal(Consts.CAR_KEYPAD_SUB), async (channel, message) =>
+        {
+            await HandleKeypadStatus(message);
+        });
+
+        Logger.LogInformation("Started");
+        await startup.SetStarted();
+    }
+
+    private async Task HandleKeypadStatus(RedisValue value)
+    {
+        var sw = Stopwatch.StartNew();
+        if (value.IsNullOrEmpty)
+        {
+            Logger.LogWarning("Received empty message");
+            return;
         }
 
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        var keypadStatus = JsonConvert.DeserializeObject<KeypadStatusDto>(value!);
+        if (keypadStatus == null)
         {
-            Logger.LogInformation("Waiting for dependencies...");
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                if (await startup.CheckDependencies())
-                    break;
-                await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
-            }
-            await startup.Start();
-
-            var sub = cacheMuxer.GetSubscriber();
-            await sub.SubscribeAsync(RedisChannel.Literal(Consts.CAR_KEYPAD_SUB), async (channel, message) =>
-            {
-                await HandleKeypadStatus(message);
-            });
-
-            Logger.LogInformation("Started");
-            await startup.SetStarted();
+            Logger.LogWarning("Received invalid message");
+            return;
         }
 
-        private async Task HandleKeypadStatus(RedisValue value)
+        Logger.LogTrace($"Received keypad status: {keypadStatus.DeviceAppId} Count={keypadStatus.LedStates.Count}");
+
+        // Reset time to server time to prevent timeouts when data is being updated.
+        keypadStatus.Timestamp = DateTime.UtcNow;
+
+        var db = cacheMuxer.GetDatabase();
+
+        var key = string.Format(Consts.KEYPAD_STATUS, keypadStatus.DeviceAppId);
+        var buttonEntries = new List<HashEntry>();
+        foreach (var bStatus in keypadStatus.LedStates)
         {
-            var sw = Stopwatch.StartNew();
-            var keypadStatus = JsonConvert.DeserializeObject<KeypadStatusDto>(value);
-
-            Logger.LogTrace($"Received keypad status: {keypadStatus.DeviceAppId} Count={keypadStatus.LedStates.Count}");
-
-            // Reset time to server time to prevent timeouts when data is being updated.
-            keypadStatus.Timestamp = DateTime.UtcNow;
-
-            var db = cacheMuxer.GetDatabase();
-
-            var key = string.Format(Consts.KEYPAD_STATUS, keypadStatus.DeviceAppId);
-            var buttonEntries = new List<HashEntry>();
-            foreach (var bStatus in keypadStatus.LedStates)
-            {
-                var ledjson = JsonConvert.SerializeObject(bStatus);
-                var h = new HashEntry(bStatus.ButtonNumber, ledjson);
-                buttonEntries.Add(h);
-            }
-
-            await db.HashSetAsync(key, buttonEntries.ToArray());
-
-            Logger.LogTrace($"Cached new keypad status for device: {keypadStatus.DeviceAppId}");
-            Logger.LogTrace($"Processed status in {sw.ElapsedMilliseconds}ms");
+            var ledjson = JsonConvert.SerializeObject(bStatus);
+            var h = new HashEntry(bStatus.ButtonNumber, ledjson);
+            buttonEntries.Add(h);
         }
+
+        await db.HashSetAsync(key, [.. buttonEntries]);
+
+        Logger.LogTrace($"Cached new keypad status for device: {keypadStatus.DeviceAppId}");
+        Logger.LogTrace($"Processed status in {sw.ElapsedMilliseconds}ms");
     }
 }
--- BigMission.KeypadServices/Program.cs
+++ BigMission.KeypadServices/Program.cs
@@ -1,62 +1,54 @@
 ﻿using BigMission.ServiceStatusTools;
 using BigMission.TestHelpers;
-using HealthChecks.UI.Client;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using NLog.Web;
 using StackExchange.Redis;
-using System.Threading.Tasks;
 
-namespace BigMission.KeypadServices
+namespace BigMission.KeypadServices;
+
+class Program
 {
-    class Program
+    async static Task Main(string[] args)
     {
-        async static Task Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Logging.ClearProviders();
+        builder.Logging.AddNLogWeb();
+
+        string redisConn = $"{builder.Configuration["REDIS_SVC"]},password={builder.Configuration["REDIS_PW"]}";
+
+        builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisConn, c => { c.AbortOnConnectFail = false; c.ConnectRetry = 10; c.ConnectTimeout = 10; }));
+        builder.Services.AddTransient<IDateTimeHelper, DateTimeHelper>();
+        builder.Services.AddSingleton<StartupHealthCheck>();
+        builder.Services.AddSingleton<ServiceTracking>();
+        builder.Services.AddHealthChecks()
+            .AddCheck<StartupHealthCheck>("Startup", tags: ["startup"])
+            .AddRedis(redisConn, tags: ["cache", "redis"])
+            .AddProcessAllocatedMemoryHealthCheck(maximumMegabytesAllocated: 1024, name: "Process Allocated Memory", tags: ["memory"]);
+        builder.Services.AddControllers();
+        builder.Services.AddHostedService<Application>();
+        builder.Services.AddHostedService(s => s.GetRequiredService<ServiceTracking>());
+
+        var app = builder.Build();
+        builder.AddRedisLogTarget();
+        var logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Main");
+        var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+        logger.LogInformation("KeypadServices Starting...");
+        logger.LogInformation(assembly.ToString());
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Logging.ClearProviders();
-            builder.Logging.AddNLogWeb();
-
-            string redisConn = $"{builder.Configuration["REDIS_SVC"]},password={builder.Configuration["REDIS_PW"]}";
-
-            builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisConn, c => { c.AbortOnConnectFail = false; c.ConnectRetry = 10; c.ConnectTimeout = 10; }));
-            builder.Services.AddTransient<IDateTimeHelper, DateTimeHelper>();
-            builder.Services.AddSingleton<StartupHealthCheck>();
-            builder.Services.AddSingleton<ServiceTracking>();
-            builder.Services.AddHealthChecks()
-                .AddCheck<StartupHealthCheck>("Startup", tags: new[] { "startup" })
-                .AddRedis(redisConn, tags: new[] { "cache", "redis" })
-                .AddProcessAllocatedMemoryHealthCheck(maximumMegabytesAllocated: 1024, name: "Process Allocated Memory", tags: new[] { "memory" });
-            builder.Services.AddControllers();
-            builder.Services.AddHostedService<Application>();
-            builder.Services.AddHostedService(s => s.GetRequiredService<ServiceTracking>());
-
-            var app = builder.Build();
-            builder.AddRedisLogTarget();
-            var logger = app.Services.GetService<ILoggerFactory>().CreateLogger("Main");
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            logger.LogInformation("KeypadServices Starting...");
-            logger.LogInformation(assembly.ToString());
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseCors(builder => builder
-               .AllowAnyHeader()
-               .AllowAnyMethod()
-               .AllowAnyOrigin());
-
-            app.UseRouting();
-            app.UseRedMistHealthCheckEndpoints();
-            app.MapControllers();
-
-            await app.RunAsync();
-            
+            app.UseDeveloperExceptionPage();
         }
+
+        app.UseCors(builder => builder
+           .AllowAnyHeader()
+           .AllowAnyMethod()
+           .AllowAnyOrigin());
+
+        app.UseRouting();
+        app.UseRedMistHealthCheckEndpoints();
+        app.MapControllers();
+
+        await app.RunAsync();
+        
     }
 }
